ea82730a4ff7a7c3cb3d98ed1112a5ec0a799992
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-08-27 20:19:18 -0300

    ex java

---
+++ DS/java/vscodeprojects/aulas/src/scanner/Nome.java
@@ -0,0 +1,15 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Nome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.println("olá, " + nome + "!");
+    }
+
+
+}
