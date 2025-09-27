118e748fa3d90874ed0e54f2170d0718a74bd264
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-09-26 21:40:05 -0300

    cadastro mobile

---
+++ Mobile/Java/AndroidStudioProjects/modularPAMIpedroatividade3/app/src/main/java/android/modularpamipedroatividade3/MainActivity.java
@@ -0,0 +1,42 @@
+package android.modularpamipedroatividade3;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText nome, email, senha;
+    Button btnCadastrar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+    }
+
+    private void initComponents() {
+        nome = findViewById(R.id.edt_nome);
+        email = findViewById(R.id.edt_email);
+        senha = findViewById(R.id.edt_senha);
+        btnCadastrar = findViewById(R.id.btn_cadastrar);
+    }
+}
