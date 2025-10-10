5f4417e32563316e3b6bc97f98867d37d9d517cf
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-10-10 20:36:02 -0300

    alteração

--- DS/POO/python O.O/src/exercicio.py
+++ DS/POO/python O.O/src/exercicio.py
@@ -1,17 +1,19 @@
 class Animal:
-    def __init__(self, raca, som):
+    def __init__(self, raca, som, cor):
         self.raca = raca
         self.som = som
+        self.cor = cor
 
     def exibir(self):
-        print(f"Raça: {self.raca}\nSom: {self.som}")
+        print(f"Raça: {self.raca}\nSom: {self.som} \nCor: {self.cor}")
 
 
 raca = input("insira a raça do animal: ")
 som = input("insria o som que o animal faz: ")
+cor = input("insira a cor do animal: ")
 
 
-cachorro = Animal(raca, som)
+cachorro = Animal(raca, som, cor)
 
 
 cachorro.exibir()
